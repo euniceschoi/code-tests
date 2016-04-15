a9fe212c660a805253b27bd91f5b15d3ba4ce030
euniceschoi <euniceschoi93@gmail.com>
2016-04-14 21:22:59 -0400

    add method that finds the palindrome from a given textfile of names

--- Ruby/palindrome/solution.rb
+++ Ruby/palindrome/solution.rb
@@ -18,10 +18,20 @@ class Name
   end
 end
 
+def find_palindrome(textfile)
+  File.open(textfile, "r").each_line do |line|
+    name = line.gsub("\n","")
+    if Name.new(name).is_palindrome?
+      p name
+    end
+  end
+end
+
+find_palindrome("names.txt")
 # Driver Test
 
-first_name = Name.new("racecar")
-p first_name.is_palindrome? == true
+# first_name = Name.new("racecar")
+# p first_name.is_palindrome? == true
 
-second_name = Name.new("meow")
-p second_name.is_palindrome? == false
+# second_name = Name.new("meow")
+# p second_name.is_palindrome? == false
