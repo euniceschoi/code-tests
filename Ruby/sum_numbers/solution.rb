5737529fe01e072d6f3d3a4f3fa756a52976f789
euniceschoi <euniceschoi93@gmail.com>
2016-04-14 22:14:24 -0400

    add solution for sum numbers

--- Ruby/sum_numbers/solution.rb
+++ Ruby/sum_numbers/solution.rb
@@ -1,1 +1,27 @@
-solution.rb
+class Number
+  def initialize(number)
+    @number = number
+  end
+
+  def sum_of_digits
+    @number.to_s.chars.map(&:to_i).reduce(:+)
+  end
+end
+
+def find_total_sum(textfile)
+  total_sum = 0
+  File.open(textfile, "r").each_line do |line|
+    number = line.gsub("\n","")
+    total_sum += Number.new(number).sum_of_digits
+  end
+  total_sum
+end
+
+p find_total_sum("numbers.txt")
+
+
+# Driver Test
+
+# test_number= Number.new(1234)
+# p test_number.sum_of_digits == 10
+
